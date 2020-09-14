388f729251ce96e6386187a0fdaee87c43704687
Karl Mabunga <karlmabunga@yahoo.com>
2020-09-14 14:06:58 -0700

    Able to add X and O onto the page when clicking on a spot.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,50 @@
 
+// Elements
+const reset = document.querySelector('button');
+const spots = document.querySelectorAll('.spot');
+console.log(spots);
+
+// game variables
+var xIsNext = true;
+var noWinner = true;
+
+// handle events
+const handleReset = (event) => {
+  console.log(event)
+}
+const handleSpotClick = (event) => {
+  const location = event.target.id;
+  const classList = event.target.classList;
+
+  if (classList[1] === 'X' || classList[1] === 'O') {
+    return;
+  }
+  if (xIsNext) {
+    event.target.classList.add('X')
+    document.getElementById(location).innerHTML = 'X'
+    console.log(event.target)
+    xIsNext = !xIsNext
+  } else {
+    event.target.classList.add('O')
+    document.getElementById(location).innerHTML = 'O'
+    console.log(event.target)
+    xIsNext = !xIsNext
+  }
+}
+
+// event listener
+
+for (const spot of spots) {
+  spot.addEventListener('click', handleSpotClick)
+}
+
+reset.addEventListener('click', handleReset)
+
+// var x = function () {
+//   inputs[0][0] = 'X';
+//   console.log(inputs);
+//   document.getElementById('one').innerHTML = "X"
+// }
+
+var inputs = ['', '', '','', '', '','', '', ''];
 
